8cba91437f14d7129323955052a369d03f081442
= <cappe@polytech.unice.fr>
2016-05-09 19:32:38 +0200

    changement nom de classe main client

--- rmi-Client/src/MainClientInscrireVoitureRegistreUniversel.java
+++ rmi-Client/src/MainClientInscrireVoitureRegistreUniversel.java
@@ -4,20 +4,17 @@ import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-public class MainClient2 {
+public class MainClientInscrireVoitureRegistreUniversel {
     
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException{
         RegistreUniversel ru =(RegistreUniversel) Naming.lookup("rmi://localhost:1098/registreuniversel");
         
         
-        MagasinVoiture mv = (MagasinVoiture) ru.lookup("magasinvoiture");
-        
-        System.out.println(mv.listerToutesVoitures());
-        
-       
-        
         ru.rebind("voiture1",new Hybride("Toyota Yaris",50000,"100 km"));
         ru.rebind("voiture2",new Voiture("Renault",15000));
+        
+        System.out.println(ru.lookup("voiture1"));
+        System.out.println(ru.lookup("voiture2"));
         
         
         
